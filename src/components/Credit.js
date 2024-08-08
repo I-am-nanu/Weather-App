3e5b9abe4416ac29df3f81a5985c92203cddc869
i-am-nanu <nanu2606patidar@gmail.com>
2024-08-08 18:22:25 +0530

    Add new file

---
+++ src/components/Credit.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      Ashish Patidar, 
+      <a href="https://github.com/shemmee/React-Weather-App">Open sourced on Github.</a>
+    </p>
+  </div>
+  );
+}
